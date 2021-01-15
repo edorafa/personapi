93219c5195497f7306d86b907666993f3f0ecc08
Rafael Sevilha <54326621+edorafa@users.noreply.github.com>
2021-01-15 18:15:55 -0300

    person operation fix-bug

--- src/main/java/com/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/com/digitalinnovation/personapi/service/PersonService.java
@@ -5,18 +5,18 @@ import com.digitalinnovation.personapi.entity.Person;
 import com.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
 
+    @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    @Autowired
+
     public MessageResponseDTO createPerson(Person person){
         Person savedPerson = personRepository.save(person);
         return MessageResponseDTO
